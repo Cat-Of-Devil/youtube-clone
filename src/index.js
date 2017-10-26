2cb46c0edc740ae9acdbeceaf092e80c32b28650
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-10-26 16:35:10 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const API_KEY = AIzaSyBKl62NN8HnmavSyL7FXNdc1Fr8YBcC0Lc;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
